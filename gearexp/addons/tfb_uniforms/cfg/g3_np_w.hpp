1cda7a7c116973393bdbb9a584947b0906e46af1
othersock93 <othersock93@gmail.com>
2026-05-15 17:40:12 +1000

    Add files via upload

---
+++ gearexp/addons/tfb_uniforms/cfg/g3_np_w.hpp
@@ -0,0 +1,183 @@
+/// gloves ///
+class tfb_g3_mc_np_g_w: tfb_g3_base_w
+{
+	scope = 2;
+	scopeArsenal = 2;
+	displayName = "Crye G3, Full (MC)";
+	class ItemInfo: ItemInfo
+	{
+		uniformClass = "tfb_g3_mc_np_g_v";
+	};
+	hiddenSelections[] = 
+	{
+		"_top", 
+		"_bottom", 
+		"_gloves", 
+		"_pads", 
+		"_boots",
+		"_patchR", 
+		"_patchL"
+	};
+	class XtdGearInfo
+	{
+		model = "tfb_g3_u";
+		top = "mc";
+		bottom = "mc";
+		sleeves = "full";
+		gloves = "on";
+		pads = "off";
+		arms = "off";
+	};
+};
+class tfb_g3_mc_r1_np_g_w: tfb_g3_base_w
+{
+	scope = 2;
+	scopeArsenal = 2;
+	displayName = "Crye G3, 1/4 Roll (MC)";
+	class ItemInfo: ItemInfo
+	{
+		uniformClass = "tfb_g3_mc_r1_np_g_v";
+	};
+	hiddenSelections[] = 
+	{
+		"_top", 
+		"_bottom", 
+		"_gloves", 
+		"_pads", 
+		"_cuffs", 
+		"_boots",
+		"_patchR", 
+		"_patchL"
+	};
+	class XtdGearInfo
+	{
+		model = "tfb_g3_u";
+		top = "mc";
+		bottom = "mc";
+		sleeves = "quarter";
+		gloves = "on";
+		pads = "off";
+		arms = "off";
+	};
+};
+class tfb_g3_mc_r2_np_g_w: tfb_g3_base_w
+{
+	scope = 2;
+	scopeArsenal = 2;
+	displayName = "Crye G3, 1/2 Roll (MC)";
+	class ItemInfo: ItemInfo
+	{
+		uniformClass = "tfb_g3_mc_r2_np_g_v";
+	};
+	hiddenSelections[] = 
+	{
+		"_top", 
+		"_bottom", 
+		"_gloves", 
+		"_pads", 
+		"cuffs", 
+		"_boots",
+		"_patchR", 
+		"_patchL"
+	};
+	class XtdGearInfo
+	{
+		model = "tfb_g3_u";
+		top = "mc";
+		bottom = "mc";
+		sleeves = "half";
+		gloves = "on";
+		pads = "off";
+		arms = "off";
+	};
+};
+/// hands ///
+class tfb_g3_mc_np_w: tfb_g3_base_w
+{
+	scope = 2;
+	scopeArsenal = 2;
+	displayName = "Crye G3, Full (MC)";
+	class ItemInfo: ItemInfo
+	{
+		uniformClass = "tfb_g3_mc_np_v";
+	};
+	hiddenSelections[] = 
+	{
+		"_top", 
+		"_bottom", 
+		"_pads", 
+		"_boots",
+		"_patchR", 
+		"_patchL"
+	};
+	class XtdGearInfo
+	{
+		model = "tfb_g3_u";
+		top = "mc";
+		bottom = "mc";
+		sleeves = "full";
+		gloves = "off";
+		pads = "off";
+		arms = "off";
+	};
+};
+class tfb_g3_mc_r1_np_w: tfb_g3_base_w
+{
+	scope = 2;
+	scopeArsenal = 2;
+	displayName = "Crye G3, 1/4 Roll (MC)";
+	class ItemInfo: ItemInfo
+	{
+		uniformClass = "tfb_g3_mc_r1_np_v";
+	};
+	hiddenSelections[] = 
+	{
+		"_top", 
+		"_bottom", 
+		"_pads", 
+		"_cuffs", 
+		"_boots",
+		"_patchR", 
+		"_patchL"
+	};
+	class XtdGearInfo
+	{
+		model = "tfb_g3_u";
+		top = "mc";
+		bottom = "mc";
+		sleeves = "quarter";
+		gloves = "off";
+		pads = "off";
+		arms = "off";
+	};
+};
+class tfb_g3_mc_r2_np_w: tfb_g3_base_w
+{
+	scope = 2;
+	scopeArsenal = 2;
+	displayName = "Crye G3, 1/2 Roll (MC)";
+	class ItemInfo: ItemInfo
+	{
+		uniformClass = "tfb_g3_mc_r2_np_v";
+	};
+	hiddenSelections[] = 
+	{
+		"_top", 
+		"_bottom", 
+		"_pads", 
+		"_cuffs", 
+		"_boots",
+		"_patchR", 
+		"_patchL"
+	};
+	class XtdGearInfo
+	{
+		model = "tfb_g3_u";
+		top = "mc";
+		bottom = "mc";
+		sleeves = "half";
+		gloves = "off";
+		pads = "off";
+		arms = "off";
+	};
+};
