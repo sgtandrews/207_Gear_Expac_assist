2d40b7e24335808e4f159cd13dfacb187a653a7b
othersock93 <othersock93@gmail.com>
2026-05-16 03:46:15 +1000

    Add files via upload

---
+++ gearexp/addons/TFL_Vests/config.cpp
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////////////////////////
+//DeRap: config.bin
+//Produced from mikero's Dos Tools Dll version 10.13
+//https://mikero.bytex.digital/Downloads
+//'now' is Mon May 11 18:31:31 2026 : 'file' last modified on Tue Jun 06 16:31:26 2023
+////////////////////////////////////////////////////////////////////
+
+#define _ARMA_
+
+class cfgMods
+{
+	author = "BLACKOUT";
+	timepacked = "1686015085";
+};
